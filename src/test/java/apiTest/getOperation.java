c74663e0d9067e7e580d42bad47ba639dc51ca36
Deepanshu <deepanshusingh8@deloitte.com>
2022-04-13 12:21:52 +0530

    file added

--- src/test/java/apiTest/getOperation.java
+++ src/test/java/apiTest/getOperation.java
@@ -6,24 +6,27 @@ import io.restassured.response.ResponseBody;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasItems;
 
 public class getOperation {
-
+     public String url ="https://gorest.co.in/public/v1/users";
     @Test
-    public void test_01() {
+    public void test_1() {
 
-        //given().get("https://gorest.co.in/public/v1/users").then().statusCode(200).log().all();
+        given().get(url).then().statusCode(200).log().all();
 
-        Response response = given().get("https://gorest.co.in/public/v1/users");
+        Response response = given().get(url);
         Assert.assertEquals(response.getStatusCode(), 200,"Response is Miss Match");
         Assert.assertEquals(response.contentType(), "application/json; charset=utf-8");
 
     }
 
-    @Test
-    public  void test_02() {
+    @Test        // validate gender from the response///
+    public  void test_2() {
         String url = "https://gorest.co.in";
         String endpoint = "/public/v1/users";
         Response response = given()
@@ -36,8 +39,21 @@ public class getOperation {
         //rbody.print();
 
 
-        given().get("https://gorest.co.in/public/v1/users").then().statusCode(200).body("data.gender",hasItems("female","male"));
-        Assert.assertEquals(response.getStatusCode(),200);
+        given().get("https://gorest.co.in/public/v1/users").
+                then().
+                statusCode(200).
+                body("data.gender",hasItems("male","female"));
+        Assert.assertEquals(response.getStatusCode(),200,"Responsce is miss Match");
         Assert.assertEquals(response.getContentType(),"application/json; charset=utf-8");
+
     }
+    @Test           //Validate the json schema for the response
+    public  void test_3() {
+        baseURI = "https://gorest.co.in";
+                given().
+                get("/public/v1/users").
+                then().
+                assertThat().body(matchesJsonSchemaInClasspath("schema.json"));
+    }
+
 }
--- src/test/java/apiTest/postOperation.java
+++ src/test/java/apiTest/postOperation.java
@@ -1,21 +1,36 @@
 package apiTest;
 
+
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 
 public class postOperation {
-    @Test
-    public  void test_03() {
-        String uri = "https://gorest.co.in/public/v1/users";
 
+    @Test
+    public  void test_05() throws IOException {
+        FileInputStream file = new FileInputStream("./ExcelTest/apiTest.xlsx");
+        XSSFWorkbook workbook = new XSSFWorkbook(file);
+        XSSFSheet sheet = workbook.getSheet("sheet1");
+
+
+        String uri = "https://gorest.co.in/public/v1/users";
+        String name = sheet.getRow(1).getCell(0).getStringCellValue();
+        String gender = sheet.getRow(1).getCell(1).getStringCellValue();
+        String email = sheet.getRow(1).getCell(2).getStringCellValue();
+        String status = sheet.getRow(1).getCell(3).getStringCellValue();
         JSONObject request = new JSONObject();
-        request.put("name","Deepanshu singh");
-        request.put("gender","male");
-        request.put("email","deee@15ce.com");
-        request.put("status","active");
+        request.put("name",name);
+        request.put("gender",gender);
+        request.put("email",email);
+        request.put("status",status);
 
         String payLoad = request.toJSONString();
         String token="1e3fbfc04d510423f16db6b1976fb8d49a339e4e486ef5006ebe118b552a2a32";
@@ -32,4 +47,6 @@ public class postOperation {
         String payLoadData = response.body().prettyPrint();
         System.out.println(payLoadData);
     }
+
+
 }
