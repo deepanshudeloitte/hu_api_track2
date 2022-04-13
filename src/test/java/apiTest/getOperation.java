6bb2e7c51bdf4aeb17044799e92c60821be455f7
Deepanshu <deepanshusingh8@deloitte.com>
2022-04-13 07:10:50 +0530

    xml file update

--- src/test/java/apiTest/getOperation.java
+++ src/test/java/apiTest/getOperation.java
@@ -1,2 +1,43 @@
-package apiTest;public class getOperation {
+package apiTest;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+
+public class getOperation {
+
+    @Test
+    public void test_01() {
+
+        //given().get("https://gorest.co.in/public/v1/users").then().statusCode(200).log().all();
+
+        Response response = given().get("https://gorest.co.in/public/v1/users");
+        Assert.assertEquals(response.getStatusCode(), 200,"Response is Miss Match");
+        Assert.assertEquals(response.contentType(), "application/json; charset=utf-8");
+
+    }
+
+    @Test
+    public  void test_02() {
+        String url = "https://gorest.co.in";
+        String endpoint = "/public/v1/users";
+        Response response = given()
+                .baseUri(url)
+                .contentType(ContentType.JSON)
+                .when()
+                .get(endpoint)
+                .then().extract().response();
+        ResponseBody rbody = response.getBody();
+        //rbody.print();
+
+
+        given().get("https://gorest.co.in/public/v1/users").then().statusCode(200).body("data.gender",hasItems("female","male"));
+        Assert.assertEquals(response.getStatusCode(),200);
+        Assert.assertEquals(response.getContentType(),"application/json; charset=utf-8");
+    }
 }
--- src/test/java/apiTest/postOperation.java
+++ src/test/java/apiTest/postOperation.java
@@ -1,2 +1,35 @@
-package apiTest;public class postOperation {
+package apiTest;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+public class postOperation {
+    @Test
+    public  void test_03() {
+        String uri = "https://gorest.co.in/public/v1/users";
+
+        JSONObject request = new JSONObject();
+        request.put("name","Deepanshu singh");
+        request.put("gender","male");
+        request.put("email","deee@15ce.com");
+        request.put("status","active");
+
+        String payLoad = request.toJSONString();
+        String token="1e3fbfc04d510423f16db6b1976fb8d49a339e4e486ef5006ebe118b552a2a32";
+        String authToken = "Bearer "+token;
+
+        RequestSpecification requestSpecification = RestAssured.given()
+                .header("Authorization",authToken)
+                .contentType("application/json")
+                .body(payLoad);
+
+        Response response = requestSpecification.post(uri);
+        int statusCode = response.getStatusCode();
+        System.out.println(statusCode);
+        String payLoadData = response.body().prettyPrint();
+        System.out.println(payLoadData);
+    }
 }
